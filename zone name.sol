30944f456d38c367c6b39d40ba37e4b85f74acd3
sawtha-as <131967427+sawtha-as@users.noreply.github.com>
2023-10-30 14:36:02 +0530

    Delete zone name.sol

--- zone name.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract ZoneNameContract {
-    function getZoneName() public pure returns (string memory) {
-        return "Zone 20";
-    }
-}
