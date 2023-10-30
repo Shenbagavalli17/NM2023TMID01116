8b1378aa1198fd17254ebf69a8aa8e3d0ef6dbbc
sawtha-as <131967427+sawtha-as@users.noreply.github.com>
2023-10-30 14:21:49 +0530

    Add files via upload

---
+++ zone name.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ZoneNameContract {
+    function getZoneName() public pure returns (string memory) {
+        return "Zone 20";
+    }
+}
